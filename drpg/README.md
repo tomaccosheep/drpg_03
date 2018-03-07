00411ceb3ee7fef9f04532d208a8957b90a08403
Al Burns <al.burns.email@gmail.com>
2018-03-07 11:38:48 -0800

    fixed mysql volume, worked with containers

---
+++ drpg/README.md
@@ -0,0 +1,8 @@
+# drpg\_03
+## Setup
+
+First, customize the password in settings.py and in /scripts/CREATE.USER.SQL
+
+Then, change the secret key in settings.py
+
+Then run docker compose up
---
+++ drpg/make_dj/admin.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from make_dj.models import Play_Project
+admin.site.register(Play_Project)
+# Register your models here.
