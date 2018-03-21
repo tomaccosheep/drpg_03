40d6aa8ca70a9f4d8a8faf73d620bae7db0c4621
al burns <al.burns.email@gmail.com>
2018-03-20 21:08:48 -0700

    added gunicorn

--- drpg/primary_container/entrypoint.sh
+++ drpg/primary_container/entrypoint.sh
@@ -2,4 +2,4 @@
 python3 manage.py makemigrations 
 python3 manage.py makemigrations make_dj
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:9020
